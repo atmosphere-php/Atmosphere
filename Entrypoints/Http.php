d27b3580ca4d2901573690e09045209898fe0e27
Romano Schoonheim <romano@consumind.nl>
2023-02-08 15:05:25 +0100

    Define kernel for console and http.

--- Entrypoints/Http.php
+++ Entrypoints/Http.php
@@ -2,5 +2,11 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
+use Atmospherephp\Framework\Http\HttpKernel;
+use Atmospherephp\Framework\Foundation\Application;
+
+$application = new Application(
+    kernel: new HttpKernel(),
+);
