89cf52dac7b435a3fda3d35d4c63e779f4a348a9
Romano Schoonheim <romano@consumind.nl>
2023-02-08 21:33:41 +0100

    Fix code style.

---
+++ Services/Commands/Routing.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Services\Commands;
+
+use Services\Commands\Controllers\ExampleCommand;
+
+return [
+    'example:command' => ExampleCommand::class,
+];
